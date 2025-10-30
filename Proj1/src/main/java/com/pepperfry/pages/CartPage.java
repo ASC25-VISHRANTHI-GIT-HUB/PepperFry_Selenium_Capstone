02955745afc580b9cb6246f6f7cf8ab4180093e3
ASC25-VISHRANTHI-GIT-HUB <avishranthi2304@gmail.com>
2025-10-30 20:34:03 +0530

    Done!

--- Proj1/src/main/java/com/pepperfry/pages/CartPage.java
+++ Proj1/src/main/java/com/pepperfry/pages/CartPage.java
@@ -14,8 +14,6 @@ public class CartPage {
     By addToCartButton = By.xpath("/html/body/app-root/main/app-vip/div/div[2]/div[2]/div[2]/section/div[6]/pf-vip-cta/div/div/div[1]/pf-button/button");
     By goToCartButton = By.xpath("/html/body/app-root/pf-header/header/div[2]/pf-header-main/div/div[1]/div/div[4]/pf-header-icons/div/div[4]/a/img");
     By proceedButton = By.xpath("/html/body/app-root/section/main/pf-cart/section/section/pf-cart-deskop-sticky-footer/div/div/div/div[2]/pf-button/button");
-//    By productName = By.xpath("//div[contains(@class,'cart-item') or contains(@class,'cartProduct')]//a");
-//    By productPrice = By.xpath("//div[contains(@class,'cart-item') or contains(@class,'cartProduct')]//span[contains(@class,'price') or contains(@class,'final-price')]");
 
     public CartPage(WebDriver driver) {
         this.driver = driver;
@@ -56,28 +54,5 @@ public class CartPage {
             return false;
         }
     }
-    // Step 3: Verify product is in cart
-//    public boolean verifyProductInCart() {
-//        try {
-//            WebElement item = wait.until(ExpectedConditions.visibilityOfElementLocated(cartItem));
-//            String name = "";
-//            String price = "";
-//
-//            try {
-//                name = driver.findElement(productName).getText();
-//            } catch (Exception ignored) {}
-//
-//            try {
-//                price = driver.findElement(productPrice).getText();
-//            } catch (Exception ignored) {}
-//
-//            System.out.println("Product in cart verified successfully:");
-//            System.out.println("Product Name: " + name);
-//            System.out.println("Product Price: " + price);
-//            return item.isDisplayed();
-//        } catch (Exception e) {
-//            System.out.println("No product found in cart: " + e.getMessage());
-//            return false;
-//        }
-//    }
 }
+
